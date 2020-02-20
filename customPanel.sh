032700a232ad599a05046f79e121e82c3f3b51ba
KellyShyno <nothing@mail.ru>
2020-02-20 09:46:50 +0300

    second commit

--- customPanel.sh
+++ customPanel.sh
@@ -5,6 +5,7 @@
 echo ""
 echo -e "\e[1;32mEnter of the number to change the panel:\e[0m";
 echo ""
+echo -e "\e[1;36m0.\e[0m\e[1;33mDefaultPanel\e[0m"
 echo -e "\e[1;36m1.\e[0m\e[1;33mFirstPanel\e[0m"
 echo -e "\e[1;36m2.\e[0\e[1;33mSecondPanel\e[0m"
 echo ""
@@ -12,17 +13,25 @@ echo -e "\e[1;32mselect:\e[0m"
 #select custom panel
   read i
 
-   if [[ $i == 1 || $i == 2 ]];
+   if [[ $i == 0 || $i == 1 || $i == 2 ]];
     then
 echo ""
 echo -e "\e[1;33mstarting to install\e[0m" $i "\e[1;33mpanel\e[0m"
 #note
+
+   if [ $i == 0 ];
+    then
+rm -rf $HOME/.termux/termux.properties
+echo -e "\e[1;32mDefault panel installed\e[0m"
+ fi
+
    if [ $i == 1 ];
     then
 rm -rf $HOME/.termux
 bash ./data/panelFirst.sh
 echo -e "\e[1;32mFirst panel installed\e[0m"
  fi
+
    if [ $i == 2 ];
     then
 rm -rf $HOME/.termux
@@ -30,7 +39,7 @@ bash ./data/panelSecond.sh
 echo -e "\e[1;32mSecond panel installed\e[0m"
  fi
 echo ""
-echo -e "\e[1;36mopen new tab to see the changes of panel\e[0m"
+echo -e "\e[1;36mrestart termux to see the changes of panel\e[0m"
 echo -e "\e[1;36mthat's awesome, isn't it?) (&_&)\e[0m"
 
    else
