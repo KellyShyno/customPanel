3c2cfdc7b97a36f5484fa2c031862511608c618f
KellyShyno <kellyshyno@bk.ru>
2020-05-18 19:53:45 +0300

    eighth commit

--- customPanel.sh
+++ customPanel.sh
@@ -2,51 +2,70 @@
 #KellyShyno
 #customPanel
 
-echo ""
-echo -e "\e[1;31mcustomPanel\e[0m""\e[1;35m(&_&)\e[0m"
-echo -e "\e[1;31mversion: 1.1.1\e[0m"
-echo -e "\e[1;33mvk: https://vk.com/linuxkelly\e[0m"
-echo ""
-echo -e "\e[1;32mEnter of the number to change the panel:\e[0m";
-echo ""
-echo -e "\e[1;36m0.\e[0m\e[1;33mDefaultPanel\e[0m"
-echo -e "\e[1;36m1.\e[0m\e[1;33mFirstPanel\e[0m"
-echo -e "\e[1;36m2.\e[0\e[1;33mSecondPanel\e[0m"
-echo ""
-echo -e "\e[1;32mselect:\e[0m"
-#select custom panel
-  read i
+inst() {
+clear
+sleep 0.34;
+echo "";
+echo -e "\e[1;31m $name\e[0m" "\e[1;36minstalled\e[0m";
+echo "";
+echo -e "\e[1;35mrestart termux to see the changes of the panel\e[0m";
+echo "";
+sleep 0.5;
+}
 
-   if [[ $i == 0 || $i == 1 || $i == 2 ]];
-    then
-echo ""
-echo -e "\e[1;33mstarting to install\e[0m" $i "\e[1;33mpanel\e[0m"
-#note
 
-   if [ $i == 0 ];
-    then
+
+
+shyno() {
+clear
+sleep 0.16;
+echo "";
+echo -e '\e[1;31m                _                  ____                  _\e[0m';
+echo -e '\e[1;31m  ___ _   _ ___| |_ ___  _ __ ___ |  _ \ __ _ _ __   ___| |\e[0m';
+echo -e '\e[33m / __| | | / __| __/ _ \| |_ \ _ \| |_) / _` | |_ \ / _ \ |\e[0m';
+echo -e '\e[33m| (__| |_| \__ \ || (_) | | | | | |  __/ (_| | | | |  __/ |\e[0m';
+echo -e '\e[1;31m \___|\__,_|___/\__\___/|_| |_| |_|_|   \__,_|_| |_|\___|_|\e[0m';
+echo "";
+echo -e "\e[1;31mversion: 2.1.1\e[0m";
+echo -e "\e[1;31mvk:\e[0m" "\e[1;4;35mhttps://vk.com/linuxkelly\e[0m";
+echo "";
+echo -e "\e[1;33mEnter of the number to change the panel:\e[0m";
+echo "";
+echo -e '\e[1;31m| \e[0m'"\e[1;36m0\e[0m"'\e[1;31m | \e[0m'"\e[1;36mDefaultPanel\e[0m";
+echo -e '\e[1;31m| \e[0m'"\e[1;36m1\e[0m"'\e[1;31m | \e[0m'"\e[1;36mFirstPanel\e[0m";
+echo -e '\e[1;31m| \e[0m'"\e[1;36m2\e[0m"'\e[1;31m | \e[0m'"\e[1;36mSecondPanel\e[0m";
+echo -e '\e[1;35m| \e[0m'"\e[1;31mx\e[0m"'\e[1;35m | \e[0m'"\e[1;36mexit\e[0m";
+echo ""
+echo -en "\e[1;36m (\e[0m""\e[1;31m@\e[0m""\e[1;36m.\e[0m""\e[1;31m@\e[0m";
+echo -en "\e[1;36m)\e[0m" "\e[1;31m> \e[0m";
+ read pan
+
+  if [[ $pan == 0 ]]; then
 rm -rf $HOME/.termux/termux.properties
-echo -e "\e[1;32mDefault panel installed\e[0m"
- fi
+name="DefaultPanel";
+inst;
 
-   if [ $i == 1 ];
-    then
+  elif [[ $pan == 1 ]]; then
 rm -rf $HOME/.termux
-bash ./data/panelFirst.sh
-echo -e "\e[1;32mFirst panel installed\e[0m"
- fi
+bash ./data/panels/panelFirst.sh
+name="FirstPanel";
+inst;
 
-   if [ $i == 2 ];
-    then
+  elif [[ $pan == 2 ]]; then
 rm -rf $HOME/.termux
-bash ./data/panelSecond.sh
-echo -e "\e[1;32mSecond panel installed\e[0m"
- fi
-echo ""
-echo -e "\e[1;36mrestart termux to see the changes of panel\e[0m"
-echo -e "\e[1;36mthat's awesome, isn't it?)\e[0m""\e[1;35m(&_&)\e[0m"
+bash ./data/panels/panelSecond.sh
+name="SecondPanel";
+inst;
 
-   else
-echo -e "\e[1;31mERROR: enter the correct number\e[0m"
-    fi
-#nicely work(just joke)
+  elif [[ $pan == "x" ]]; then
+echo "";
+exit 0;
+
+  else
+clear
+echo -e "\e[1;31mERROR: write the correct number..\e[0m";
+sleep 0.7;
+shyno;
+  fi;
+}
+shyno;
