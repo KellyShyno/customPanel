4bf1fe03f813ce6b96ead01da1f765843452dee9
KellyShyno <kellyshyno@bk.ru>
2020-06-03 10:51:00 +0300

    refactoring

--- customPanel.sh
+++ customPanel.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #KellyShyno
 #customPanel
+#version: 2.2.2
 
 changeMod() {
 chmod -R a+rx *;
@@ -9,8 +10,25 @@ changeMod;
 
 
 
+banner()
+{
+clear
+sleep 0.16;
+echo "";
+echo -e '\e[1;31m                _                  ____                  _\e[0m';
+echo -e '\e[1;31m  ___ _   _ ___| |_ ___  _ __ ___ |  _ \ __ _ _ __   ___| |\e[0m';
+echo -e '\e[33m / __| | | / __| __/ _ \| |_ \ _ \| |_) / _` | |_ \ / _ \ |\e[0m';
+echo -e '\e[33m| (__| |_| \__ \ || (_) | | | | | |  __/ (_| | | | |  __/ |\e[0m';
+echo -e '\e[1;31m \___|\__,_|___/\__\___/|_| |_| |_|_|   \__,_|_| |_|\___|_|\e[0m';
+echo "";
+echo -e "\e[1;31mversion: 2.2.2\e[0m";
+echo -e "\e[1;31mvk:\e[0m" "\e[1;4;35mhttps://vk.com/linuxkelly\e[0m";
+echo "";
+}
 
-inst() {
+
+
+installed() {
 clear
 sleep 0.34;
 echo "";
@@ -23,57 +41,25 @@ sleep 0.5;
 
 
 
-
-shyno() {
-clear
-sleep 0.16;
-echo "";
-echo -e '\e[1;31m                _                  ____                  _\e[0m';
-echo -e '\e[1;31m  ___ _   _ ___| |_ ___  _ __ ___ |  _ \ __ _ _ __   ___| |\e[0m';
-echo -e '\e[33m / __| | | / __| __/ _ \| |_ \ _ \| |_) / _` | |_ \ / _ \ |\e[0m';
-echo -e '\e[33m| (__| |_| \__ \ || (_) | | | | | |  __/ (_| | | | |  __/ |\e[0m';
-echo -e '\e[1;31m \___|\__,_|___/\__\___/|_| |_| |_|_|   \__,_|_| |_|\___|_|\e[0m';
-echo "";
-echo -e "\e[1;31mversion: 2.1.1\e[0m";
-echo -e "\e[1;31mvk:\e[0m" "\e[1;4;35mhttps://vk.com/linuxkelly\e[0m";
-echo "";
+panel() {
+banner;
 echo -e "\e[1;33mEnter of the number to change the panel:\e[0m";
 echo "";
 echo -e '\e[1;31m| \e[0m'"\e[1;36m0\e[0m"'\e[1;31m | \e[0m'"\e[1;36mDefaultPanel\e[0m";
-echo -e '\e[1;31m| \e[0m'"\e[1;36m1\e[0m"'\e[1;31m | \e[0m'"\e[1;36mFirstPanel\e[0m";
-echo -e '\e[1;31m| \e[0m'"\e[1;36m2\e[0m"'\e[1;31m | \e[0m'"\e[1;36mSecondPanel\e[0m";
+echo -e '\e[1;31m| \e[0m'"\e[1;36m1\e[0m"'\e[1;31m | \e[0m'"\e[1;36mClassicPanel\e[0m";
+echo -e '\e[1;31m| \e[0m'"\e[1;36m2\e[0m"'\e[1;31m | \e[0m'"\e[1;36mCodingPanel\e[0m";
 echo -e '\e[1;35m| \e[0m'"\e[1;31mx\e[0m"'\e[1;35m | \e[0m'"\e[1;36mexit\e[0m";
 echo ""
 echo -en "\e[1;36m (\e[0m""\e[1;31m@\e[0m""\e[1;36m.\e[0m""\e[1;31m@\e[0m";
 echo -en "\e[1;36m)\e[0m" "\e[1;31m> \e[0m";
  read pan
 
-  if [[ $pan == 0 ]]; then
-rm -rf $HOME/.termux/termux.properties
-name="DefaultPanel";
-inst;
-
-  elif [[ $pan == 1 ]]; then
-rm -rf $HOME/.termux
-bash ./data/panels/panelFirst.sh
-name="FirstPanel";
-inst;
-
-  elif [[ $pan == 2 ]]; then
-rm -rf $HOME/.termux
-bash ./data/panels/panelSecond.sh
-name="SecondPanel";
-inst;
-
-  elif [[ $pan == "x" ]]; then
-echo "";
-exit 0;
-
-  else
-clear
-echo -e "\e[1;31mERROR: write the correct number..\e[0m";
-sleep 0.7;
-shyno;
-  fi;
+    case $pan in
+0) rm -rf $HOME/.termux/termux.properties; name="DefaultPanel"; installed; ;;
+1) rm -rf $HOME/.termux; bash ./data/panels/ClassicPanel.sh; name="ClassicPanel"; installed; ;;
+2) rm -rf $HOME/.termux; bash ./data/panels/CodingPanel.sh; name="CodingPanel"; installed; ;;
+"x") echo ""; exit 0; ;;
+*) clear; echo -e "\e[1;31mERROR: write the correct number..\e[0m"; sleep 0.7; panel; ;;
+    esac;
 }
-shyno;
+panel;
